62d1f58de1da116fe039548e362985518e7b91a6
Hashim El-Alej <hashimmelalej@gmail.com>
2023-11-22 12:00:11 +0000

    task-9 fixed

--- models/app.model.js
+++ models/app.model.js
@@ -98,23 +98,16 @@ exports.updateVotes = (votes, id) => {
 };
 
 exports.removeComment = (id) => {
-
     return db.query(`
-        SELECT * FROM comments
-        WHERE comment_id = $1;
+        DELETE FROM comments
+        WHERE comment_id = $1
     `, [id])
-        .then(({ rows }) => {
-            if (!rows.length) {
+        .then((result) => {
+            const rowCount = result.rowCount
+            if (rowCount > 0) {
+                return result.rows
+            } else {
                 return Promise.reject({ status: 404, msg: 'Comment does not exist' })
             }
-        })
-        .then(() => {
-            return db.query(`
-            DELETE FROM comments
-            WHERE comment_id = $1
-        `, [id])
-        })
-        .then(({rows}) => {
-            return rows
         })
 };
